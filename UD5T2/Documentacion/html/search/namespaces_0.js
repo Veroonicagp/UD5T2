0192f8839de57a7216aed22106e52f8d9da78ba5
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2024-02-14 09:06:13 +0100

    subida

---
+++ UD5T2/Documentacion/html/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ud5t2_0',['UD5T2',['../namespace_u_d5_t2.html',1,'']]]
+];
---
+++ UD5T2/Documentacion/html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "acmou",
+  1: "am",
+  2: "u",
+  3: "am",
+  4: "amo",
+  5: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Clases",
+  2: "Espacios de nombres",
+  3: "Archivos",
+  4: "Funciones",
+  5: "Variables"
+};
+
